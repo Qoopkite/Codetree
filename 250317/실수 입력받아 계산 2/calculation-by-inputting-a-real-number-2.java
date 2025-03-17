3781d7d54177438c53aa7d6dc1c38f9467026df0
배새연 <163410353+Qoopkite@users.noreply.github.com>
2025-03-17 14:14:34 +0900

    Title: 실수 입력받아 계산 2, Time: 146ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 250317/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner scanner = new Scanner(System.in);
+
+        double a = scanner.nextDouble();
+        a=a+1.5;
+
+        System.out.printf("%.2f",a);
+    }
+}
