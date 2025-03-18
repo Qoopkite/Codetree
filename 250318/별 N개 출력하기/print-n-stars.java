e83ef9028433a5496e01d3454d237724b1ac48e0
배새연 <163410353+Qoopkite@users.noreply.github.com>
2025-03-18 15:36:39 +0900

    Title: 별 N개 출력하기, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250318/별 N개 출력하기/print-n-stars.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        int i =0;
+        while(i<N){
+            System.out.println("*");
+            i++;
+        }
+    }
+}
