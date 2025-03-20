c32d6e04cc7e48f97ed425fbfd96631b33fd292e
배새연 <163410353+Qoopkite@users.noreply.github.com>
2025-03-20 11:23:32 +0900

    Title: 열에 따라 감소하는 정사각형, Time: 114ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250320/열에 따라 감소하는 정사각형/a-square-that-decreases-with-heat.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        
+        for(int i =0; i<N; i++){
+            for(int j = N; j >0; j--){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
