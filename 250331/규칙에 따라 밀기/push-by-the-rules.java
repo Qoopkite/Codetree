af8ac60dfee744b02fbff3d790a071bb3007cb30
배새연 <163410353+Qoopkite@users.noreply.github.com>
2025-03-31 15:58:09 +0900

    Title: 규칙에 따라 밀기, Time: 118ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250331/규칙에 따라 밀기/push-by-the-rules.java
+++ 250331/규칙에 따라 밀기/push-by-the-rules.java
@@ -20,11 +20,15 @@ public class Main {
         
         if(L>R){//L만큼 왼쪽밀기
         int x=L-R;
-        A = A.substring(x)+A.substring(0, x);
+        for(int i=0; i<x; i++){
+        A = A.substring(1)+A.substring(0, 1);
+        }
         System.out.print(A);
         }else if(R<L){//R만큼 오른쪽밀기
         int x=R-L;
-        A = A.substring(A.length()-x)+A.substring(0, A.length()-x);
+        for(int i=0; i<x; i++){
+        A = A.substring(A.length()-1)+A.substring(0, A.length()-1);
+        }
         System.out.print(A);
         }else{//L과 R개수 동일 할 때
         System.out.print(A);
