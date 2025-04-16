afdefe2c84a93f3e80f65a7e803b05df991c9311
배새연 <163410353+Qoopkite@users.noreply.github.com>
2025-04-16 16:47:11 +0900

    Title: 함수를 이용한 369 게임, Time: 205ms, Memory: 19MB, Status: Passed - Codetree

--- 250416/함수를 이용한 369 게임/369-games-using-functions.java
+++ 250416/함수를 이용한 369 게임/369-games-using-functions.java
@@ -1,24 +1,26 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int A = sc.nextInt();
         int B = sc.nextInt();
-        int cnt=0;
-        
-        for(int i=A; i<=B; i++){
-            if(MagicNum(i)){
+        int cnt = 0;
+
+        for (int i = A; i <= B; i++) {
+            if (isMagicNumber(i)) {
                 cnt++;
             }
-            
         }
         System.out.println(cnt);
     }
 
-    public static boolean MagicNum(int N){
-        int x = N/10;
-        int y= N%10;
-        return N%3==0 || x==3 || x==6 || x==9 || y==3 || y==6 || y==9 ;
+    public static boolean isMagicNumber(int N) {
+        // 3의 배수면 true
+        if (N % 3 == 0) return true;
 
+        // 문자열로 변환 후 포함 여부 확인
+        String str = Integer.toString(N);
+        return str.contains("3") || str.contains("6") || str.contains("9");
     }
 }
